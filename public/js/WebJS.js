4fb8b2c5efa2365329139aee615094501b5ab87e
EunhoKim219 <eunho.kim@stonybrook.edu>
2017-02-12 17:30:47 -0500

    first commit

---
+++ public/js/WebJS.js
@@ -0,0 +1,11 @@
+function initFireBase() {
+    // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCgHmztKhHlRhg-6CNMdQHGrVEl_8pp2hs",
+    authDomain: "baileywei-7ca5d.firebaseapp.com",
+    databaseURL: "https://baileywei-7ca5d.firebaseio.com",
+    storageBucket: "baileywei-7ca5d.appspot.com",
+    messagingSenderId: "124772457702"
+  };
+  firebase.initializeApp(config);
+}
